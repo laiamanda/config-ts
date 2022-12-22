8e32950c95584e139db67b6eb5597d3f9f1226fc
laiamanda <40805270+laiamanda@users.noreply.github.com>
2022-12-21 20:25:37 -0500

    Add /dist

---
+++ dist/app.js
@@ -0,0 +1,7 @@
+"use strict";
+const userName = 'Amanda';
+console.log(userName);
+// const button = document.querySelector('button');
+// button.addEventListener('click', () => {
+//     console.log('clicked');
+// })
